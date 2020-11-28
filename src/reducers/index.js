9a2c73aedb56f73ddd847d94c5fab531717a3f11
Borrison <borissoto@gmail.com>
2020-11-28 17:53:29 -0400

    actions and reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,24 @@
+const songsReducer = () => {
+  return [
+    {
+      title: 'No  Scrubs',
+      duration: '4:05',
+    },
+    {
+      title: 'Macarena',
+      duration: '2.40',
+    },
+    {
+      title: 'Smells like teen spirit',
+      duration: '3.23',
+    },
+    { title: 'All star', duration: '1.45' },
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === 'SONG_SELECTED') {
+    return action.payload;
+  }
+  return selectedSong;
+};
